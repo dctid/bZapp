63b8369a1beb989ddad520fafab31e850edb48d0
Chris Dorman <cdorman1@ford.com>
2021-01-22 14:20:38 -0500

    Adding deploy to actions

--- dashboard.go
+++ dashboard.go
@@ -8,7 +8,7 @@ import (
 // Dashboard returns a dashboard HTML page
 func Dashboard(ctx context.Context, e events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	return events.APIGatewayProxyResponse{
-		Body: "<html><body><h1>bZapp dashboard</h1></body></html>\n",
+		Body: "<html><body><h1>bZapp dashboard 2</h1></body></html>\n",
 		Headers: map[string]string{
 			"Content-Type": "text/html",
 		},
